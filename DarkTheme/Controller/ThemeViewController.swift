f87a039a1e85b4d8f68a2a91b552eb5182545bd1
Arkadijs Makarenko <arkadijs.makarenko@gmail.com>
2023-04-03 20:45:48 +0300

    before home task

---
+++ DarkTheme/Controller/ThemeViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ThemeViewController.swift
+//  DarkTheme
+//
+//  Created by Arkadijs Makarenko on 03/04/2023.
+//
+
+import UIKit
+
+class ThemeViewController: UIViewController {
+
+    
+    @IBOutlet weak var darkThemeButton: UIButton!
+    var mainText: String = "Dark Theme on"
+    var darkIsOn: Bool = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad from ViewController")
+        darkThemeButton.layer.cornerRadius = 8
+        
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func darkThemeButtonTapped(_ sender: Any) {
+        
+        darkThemeIsOn(isOn: darkIsOn)
+        
+    }
+    
+    func darkThemeIsOn(isOn: Bool){
+        //mainText = "Dark Theme on"
+        darkThemeButton.setTitle(mainText, for: .normal)
+        darkThemeButton.setTitleColor(UIColor.black, for: .normal)
+        
+        navigationItem.title = mainText
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.red]
+        view.backgroundColor = UIColor.black
+    }
+    
+    
+}
+
