9788de5e2a41357aa709492dc154ac7ef95fc4f5
Arkadijs Makarenko <arkadijs.makarenko@gmail.com>
2023-04-05 15:48:49 +0300

    adding assets

--- DarkTheme/Controller/ThemeViewController.swift
+++ DarkTheme/Controller/ThemeViewController.swift
@@ -11,6 +11,7 @@ class ThemeViewController: UIViewController {
 
     
     @IBOutlet weak var darkThemeButton: UIButton!
+    
     var mainText: String = "Dark Theme on"
     var darkIsOn: Bool = false
     
@@ -19,23 +20,35 @@ class ThemeViewController: UIViewController {
         print("viewDidLoad from ViewController")
         darkThemeButton.layer.cornerRadius = 8
         
+        darkThemeIsOn(isOn: darkIsOn)
         // Do any additional setup after loading the view.
     }
 
     @IBAction func darkThemeButtonTapped(_ sender: Any) {
-        
+
         darkThemeIsOn(isOn: darkIsOn)
         
     }
     
+    @IBAction func plusButtonTapped(_ sender: Any) {
+        basicAlert(title: "Add New?", message: "Do you want to add new...?")
+    }
+    
+    @IBAction func infoButtonTapped(_ sender: Any) {
+        basicActionAlert(title: "Action Sheet is On!", message: "All good here")
+    }
+    
+    
     func darkThemeIsOn(isOn: Bool){
-        //mainText = "Dark Theme on"
+        mainText = isOn ? "Dark Theme on" : "Dark Theme off"
+        
         darkThemeButton.setTitle(mainText, for: .normal)
-        darkThemeButton.setTitleColor(UIColor.black, for: .normal)
+        darkThemeButton.setTitleColor(isOn ? UIColor.black : UIColor.white, for: .normal)
         
         navigationItem.title = mainText
-        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.red]
-        view.backgroundColor = UIColor.black
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: isOn ? UIColor.black : UIColor.white]
+        view.backgroundColor = !isOn ? UIColor.black : UIColor.white
+        darkIsOn.toggle()
     }
     
     
---
+++ DarkTheme/Extension/Extention.swift
@@ -0,0 +1,30 @@
+//
+//  Extention.swift
+//  DarkTheme
+//
+//  Created by Arkadijs Makarenko on 05/04/2023.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func basicAlert(title: String?, message: String?){
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            
+            self.present(alert, animated: true)
+        }
+    }
+    
+    func basicActionAlert(title: String?, message: String?){
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            
+            self.present(alert, animated: true)
+        }
+    }
+    
+}
